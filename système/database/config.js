b9edd84090d7e8180ccf8c947b5f493e4d6dec33
yourname202 <chantalmbayo121@gmail.com>
2025-12-24 05:52:59 +0200

    Add configuration for bot settings and messages

---
+++ système/database/config.js
@@ -0,0 +1,12 @@
+//========HELO FRIEND========//
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; // jan ubah ntr erorr
+global.publik = true
+global.owner = ["243982355070"] 
+global.namabot = 'LukasCrasher'
+//======================
+global.mess = { 
+owner: '*waduhh!, lu bukan owner gw bg*',
+premium: '*anda bukan user premium*',
+succes: '*done bang*'
+}
+//======================
